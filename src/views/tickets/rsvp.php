a847b7fcbca7334f773d1012d094b88aa6deef61
Didier Grand <didier.grand@gmail.com>
2025-02-19 07:55:11 +0100

    Revert messages tests

--- src/views/tickets/rsvp.php
+++ src/views/tickets/rsvp.php
@@ -81,7 +81,7 @@ if ( ! $already_rendered ) {
 			foreach ( $messages as $message ) {
 				?>
 				<div class="tribe-rsvp-message tribe-rsvp-message-<?php echo esc_attr( $message->type ); ?>">
-					1 <?php echo esc_html( $message->message ); ?>
+					<?php echo esc_html( $message->message ); ?>
 				</div>
 				<?php
 			}//end foreach
